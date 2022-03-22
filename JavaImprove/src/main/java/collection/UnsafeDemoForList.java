114c959a60b9661eb1f8cdf4ce1fb825d3a5abfa
jinman.zhu@hand-china.com <jinman.zhu@hand-china.com>
2022-03-22 18:31:19 +0800

    List

---
+++ JavaImprove/src/main/java/collection/UnsafeDemoForList.java
@@ -0,0 +1,15 @@
+package collection;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author kewudenao 2022-03-22 17:30
+ **/
+public class UnsafeDemoForList {
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList("a","c","b","d","f");
+		//list.forEach(System.out::println);
+
+	}
+}
